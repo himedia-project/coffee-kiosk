a1f1fc7ced178f101d53aa861b2c767605350980
seonggon do <mooh2jj@naver.com>
2024-11-05 15:43:49 +0900

    MenuItem 디자인 수정

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -4,18 +4,21 @@ import itemData from "../../data/itemData";
 
 const MenuItem = ({ category }) => {
   return (
-    <div>
-      <h2>{category} 메뉴</h2>
-      <ul>
+    <div className="text-center p-5 bg-gray-100">
+      <h2 className="text-2xl font-bold mb-5">{category} 메뉴</h2>
+      <ul className="flex flex-wrap justify-center list-none p-0">
         {itemData[category]?.map((item) => (
-          <li key={item.id}>
-            <Link to={`/options/${item.id}`}>
-              {item.name} |{" "}
-              <img
-                src={item.url}
-                alt={item.name}
-                style={{ width: "70px", height: "auto" }}
-              />
+          <li
+            key={item.id}
+            className="m-2 border border-gray-300 rounded-lg overflow-hidden w-40 bg-white transition-transform transform hover:scale-105"
+          >
+            <Link
+              to={`/options/${item.id}`}
+              className="no-underline text-gray-800 flex flex-col items-center p-3"
+            >
+              <img src={item.url} alt={item.name} className="w-24 h-auto" />
+              <span className="text-lg mt-2">{item.name}</span>
+              <span className="text-sm text-gray-600">{item.price}원</span>
             </Link>
           </li>
         ))}
