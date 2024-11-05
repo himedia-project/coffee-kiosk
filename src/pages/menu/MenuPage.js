38e06fe300c1dff2828331cc780e0b1e0e0e4958
seonggon do <mooh2jj@naver.com>
2024-11-05 15:22:28 +0900

    처음 MenuPage가 Mounted가 되면 default category는 "커피"로

--- src/pages/menu/MenuPage.js
+++ src/pages/menu/MenuPage.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Category from "../../components/menu/Category";
 import MenuItem from "../../components/menu/MenuItem";
 import Header from "../../layouts/Header";
 
 const MenuPage = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
+
+  useEffect(() => {
+    setSelectedCategory("커피"); // 기본 카테고리 설정
+  }, []); // 컴포넌트가 처음 마운트될 때만 실행
 
   return (
     <div>
