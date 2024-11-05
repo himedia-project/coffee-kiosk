0aceabafe0deee899ca34df7b02f72d5d032b2fa
seonggon do <mooh2jj@naver.com>
2024-11-05 16:19:23 +0900

    Header에 home_icon 추가

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -6,13 +6,9 @@ const Header = () => {
     <>
       <header className="bg-green-600 text-white p-4 flex items-center justify-between">
         <Link to="/" className="text-xl font-bold">
-          <img
-            src="home_icon.png"
-            alt="Home"
-            className="inline-block w-6 h-6"
-          />{" "}
-          Mega I's Cafe KIOSK
+          <img src="home.png" alt="Home" className="inline-block w-6 h-6" />{" "}
         </Link>
+        <h1 className="text-2xl text-center flex-1">Mega I's Cafe KIOSK</h1>
       </header>
     </>
   );
