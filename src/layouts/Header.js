dcfe9aab3d6e8c35f43442e695bc94a7237dd939
seonggon do <mooh2jj@naver.com>
2024-11-05 15:04:47 +0900

    Header home 이동 기능 구현

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <Link to="/">Home</Link>
+      <header className="bg-green-600 text-white p-4 flex items-center justify-between">
+        <Link to="/" className="text-xl font-bold">
+          <span className="material-icons">home</span> LYJ KIOSK
+        </Link>
+      </header>
     </>
   );
 };
