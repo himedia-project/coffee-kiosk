48979d145b16cadc7ca034a75253a1642755f3d6
seonggon do <mooh2jj@naver.com>
2024-11-05 12:08:24 +0900

    Home 화면 디자인 추가

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <h1>환영합니다! 커피 키오스크</h1>
-      <p>원하시는 카테고리를 선택해주세요.</p>
-      <Link to="/category">카테고리 선택</Link>
+    <div className="flex flex-col items-center justify-center h-screen bg-green-500 text-white p-6">
+      <img src="cafe_icon.png" alt="LYJ KIOSK" className="w-24 mb-6" />
+      <h1 className="text-3xl font-bold">기다리지 말고 간편하게</h1>
+      <h2 className="text-xl mb-4">키오스크에서 "주문하세요!"</h2>
+      <button className="bg-white text-green-500 py-2 px-4 rounded-lg shadow-md hover:bg-gray-200">
+        <Link to="/category">화면을 터치해 주세요</Link>
+      </button>
     </div>
   );
 };
