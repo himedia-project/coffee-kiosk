774c4bab2fdfde91da2539ae830d398b12e2c5f7
seonggon do <mooh2jj@naver.com>
2024-11-06 14:48:43 +0900

    Header 홈이동 문구 추가

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -5,9 +5,12 @@ const Header = () => {
   return (
     <>
       <header className="bg-green-600 text-white p-4 flex items-center justify-between">
-        <Link to="/" className="text-xl font-bold">
-          <img src="home.png" alt="Home" className="inline-block w-6 h-6" />{" "}
-        </Link>
+        <div className="flex flex-row items-start">
+          <Link to="/" className="text-xl font-bold flex items-center">
+            <img src="home.png" alt="Home" className="inline-block w-6 h-6" />{" "}
+            <p className="ml-2 mt-1">처음으로</p>
+          </Link>
+        </div>
         <h1 className="text-2xl text-center font-bold flex-1">
           Mega I's Cafe KIOSK
         </h1>
