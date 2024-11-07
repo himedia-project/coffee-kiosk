79f5bb0149e190349c7c2e72a08e557ec27995d0
seonggon do <mooh2jj@naver.com>
2024-11-07 15:04:56 +0900

    헤더 제목 중앙고정

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <header className="bg-green-600 text-white p-4 flex items-center justify-between">
-        <div className="flex flex-row items-start">
+      <header className="bg-green-600 text-white p-4 relative flex items-center">
+        <div className="absolute left-4">
           <Link to="/" className="text-xl font-bold flex items-center">
             <img src="home.png" alt="Home" className="inline-block w-6 h-6" />{" "}
             <p className="ml-2 mt-1">처음으로</p>
           </Link>
         </div>
-        <h1 className="text-2xl text-center font-bold flex-1">
+        <h1 className="text-2xl font-bold w-full text-center">
           Mega I's Cafe KIOSK
         </h1>
       </header>
