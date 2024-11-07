fb0b028f08886178dc5747f44ef5c65f1763b17a
y_daniel <y_daniel@naver.com>
2024-11-07 16:58:09 +0900

    장소, 포장 이미지 추가

--- src/components/order/LocationSelect.js
+++ src/components/order/LocationSelect.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const locations = ["매장", "포장"];
+const locations = [
+  { name: "매장", image: "for_here.png" },
+  { name: "포장", image: "To_go.png" },
+];
 
 const LocationSelect = ({ onSelect, closeModal }) => {
   console.log("LocationSelect open!");
@@ -12,21 +15,27 @@ const LocationSelect = ({ onSelect, closeModal }) => {
         <h2 className="text-2xl text-center font-bold mb-4">
           장소를 선택해 주세요
         </h2>
-        <ul className="flex flex-col space-y-4">
+        <div className="flex justify-between">
+          {" "}
           {locations.map((location) => (
-            <li key={location}>
-              <button
-                className="w-full bg-green-500 text-white px-4 py-2 rounded"
-                onClick={() => {
-                  onSelect(location);
-                  closeModal();
-                }}
-              >
-                {location}
-              </button>
-            </li>
-          ))}
-        </ul>
+            <button
+              key={location.name}
+              className="flex flex-col items-center bg-green-500 text-white px-4 py-2 rounded w-40"
+              onClick={() => {
+                onSelect(location.name);
+                closeModal();
+              }}
+            >
+              {" "}
+              <img
+                src={location.image}
+                alt={location.name}
+                className="w-16 h-16 mb-2"
+              />{" "}
+              {location.name}{" "}
+            </button>
+          ))}{" "}
+        </div>
         <div className="mt-4 flex justify-center">
           <button
             className="bg-gray-300 text-gray-800 px-4 py-2 rounded"
