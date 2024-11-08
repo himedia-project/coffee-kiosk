5bdb2ca75f4ebc92d520df142e3f819ecee604f4
seonggon do <mooh2jj@naver.com>
2024-11-08 16:04:48 +0900

    스와이핑시, 슬라이드 transition 시간 설정(1200)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideLeft: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        slideRight: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(100%)" },
+        },
+      },
+      animation: {
+        "slide-left": "slideLeft 0.3s ease-in-out",
+        "slide-right": "slideRight 0.3s ease-in-out",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
