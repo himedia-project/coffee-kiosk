a10b75e455b44546e832701d56db7bd720369cb9
seonggon do <mooh2jj@naver.com>
2024-11-08 11:43:19 +0900

    터치 슬라이드 적용

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import itemData from "../../data/itemData";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+import { useSwipeable } from "react-swipeable";
 
 const MenuItem = ({ category, onItemClick }) => {
   const items = itemData[category] || [];
@@ -26,23 +27,41 @@ const MenuItem = ({ category, onItemClick }) => {
     (currentPage + 1) * itemsPerPage
   );
 
+  const handlers = useSwipeable({
+    onSwipedLeft: () => {
+      if (currentPage < totalPages - 1) {
+        nextPage();
+      }
+    },
+    onSwipedRight: () => {
+      if (currentPage > 0) {
+        prevPage();
+      }
+    },
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: false,
+  });
+
   return (
     <div className="relative max-w-screen-lg mx-auto px-4 py-6">
       <h2 className="text-2xl font-bold mb-4 text-center">{category} 메뉴</h2>
 
       <div className="relative">
-        {/* Prev Button */}
+        {/* Prev Button - 데스크톱용 */}
         {currentPage > 0 && (
           <button
             onClick={prevPage}
-            className="absolute left-0 top-1/2 -translate-y-1/2 -translate-x-4 z-10 bg-white rounded-full p-2 shadow-lg hover:bg-gray-100"
+            className="absolute left-0 top-1/2 -translate-y-1/2 -translate-x-4 z-10 bg-white rounded-full p-2 shadow-lg hover:bg-gray-100 hidden md:block"
           >
             <ChevronLeftIcon className="w-6 h-6 text-gray-600" />
           </button>
         )}
 
-        {/* Items Grid */}
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 transition-all duration-300 ease-in-out">
+        {/* Items Grid - 스와이프 기능 추가 */}
+        <div
+          {...handlers}
+          className="grid grid-cols-2 md:grid-cols-4 gap-4 transition-all duration-300 ease-in-out touch-pan-y"
+        >
           {currentItems.map((item, index) => (
             <div
               key={item.id}
@@ -101,11 +120,11 @@ const MenuItem = ({ category, onItemClick }) => {
           ))}
         </div>
 
-        {/* Next Button */}
+        {/* Next Button - 데스크톱용 */}
         {currentPage < totalPages - 1 && (
           <button
             onClick={nextPage}
-            className="absolute right-0 top-1/2 -translate-y-1/2 translate-x-4 z-10 bg-white rounded-full p-2 shadow-lg hover:bg-gray-100"
+            className="absolute right-0 top-1/2 -translate-y-1/2 translate-x-4 z-10 bg-white rounded-full p-2 shadow-lg hover:bg-gray-100 hidden md:block"
           >
             <ChevronRightIcon className="w-6 h-6 text-gray-600" />
           </button>
