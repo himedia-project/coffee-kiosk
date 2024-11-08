473c49240d061d2790f5643c0e762b886ade2fad
seonggon do <mooh2jj@naver.com>
2024-11-08 11:18:23 +0900

    홈이미지와 타이틀 이미지 가로로 정렬

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -5,17 +5,19 @@ const Header = () => {
   return (
     <>
       <header className="bg-[#c5e1bb] text-[#2d1b1b] p-4 relative flex items-center justify-center">
-        <div className="absolute left-4">
+        <div className="absolute left-4 flex items-center">
           <Link to="/" className="text-xl font-bold flex items-center">
             <img
               src="home.png"
               alt="Home"
               className="inline-block w-6 h-6 [filter:brightness(0)_saturate(100%)_invert(15%)_sepia(10%)_saturate(519%)_hue-rotate(314deg)_brightness(94%)_contrast(93%)]"
-            />{" "}
-            <p className="ml-2 mt-1 hidden sm:block">처음으로</p>
+            />
+            <p className="ml-2 hidden sm:block">처음으로</p>
           </Link>
         </div>
-        <img className="w-[350px] h-auto" src="title.png" alt="LYJ KIOSK" />
+        <div className="mt-2">
+          <img className="w-[350px]" src="title.png" alt="LYJ KIOSK" />
+        </div>
       </header>
     </>
   );
