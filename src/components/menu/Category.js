b193887f4d43b25b31286b1fb6e9ff9f32f377ef
seonggon do <mooh2jj@naver.com>
2024-11-08 11:09:06 +0900

    카테고리 space 간격 줄임

--- src/components/menu/Category.js
+++ src/components/menu/Category.js
@@ -10,12 +10,12 @@ const Category = ({ setSelectedCategory, selectedCategory }) => {
 
   return (
     <div className="bg-white p-4">
-      <ul className="flex justify-center space-x-4">
+      <ul className="flex justify-center space-x-3">
         {categories.map((category) => (
           <li key={category.name} className="flex-1">
             <button
               onClick={() => setSelectedCategory(category.name)}
-              className={`block text-lg sm:text-xl font-bold text-center py-2 px-4 rounded-lg border border-gray-300 transition w-full ${
+              className={`block text-lg sm:text-xl font-bold text-center py-2 px-2 rounded-lg border border-gray-300 transition w-full ${
                 selectedCategory === category.name
                   ? "bg-[#c5e1bb] text-[#2d1b1b]"
                   : "hover:bg-[#c5e1bb] hover:text-white"
